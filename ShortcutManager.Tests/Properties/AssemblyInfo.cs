f82070751dcb4845c3c29c82923e635a2ef132f9
Jack Dewinter <Jack.Dewinter@red-gate.com>
2019-03-17 21:05:52 +0000

    Add test project

---
+++ ShortcutManager.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ShortcutManager.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Redgate")]
+[assembly: AssemblyProduct("ShortcutManager.Tests")]
+[assembly: AssemblyCopyright("Copyright © Redgate 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b7d34e95-add8-4c5c-91d9-770774f2fd81")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
