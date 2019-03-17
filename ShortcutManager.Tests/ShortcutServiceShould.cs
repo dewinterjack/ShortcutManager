6fd7d4479727ef417876bd5e94d2171dd647571d
Jack Dewinter <Jack.Dewinter@red-gate.com>
2019-03-17 21:18:05 +0000

    Add failing test for shortcut service

---
+++ ShortcutManager.Tests/ShortcutServiceShould.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace ShortcutManager.Tests
+{
+    public class ShortcutServiceShould
+    {
+        [Test]
+        public void AddShortcutToDefaultWorkspace()
+        {
+            var shortcutService = new ShortcutService();
+            Workspace.Default.ShouldBeEmpty();
+
+            var shortcutName = "Github Profile";
+            var shortcutLink = "https://github.com/dewinterjack";
+            shortcutService.AddShortcut(shortcutName, shortcutLink);
+
+            Workspace.Default.Count.ShouldBe(1);
+            Workspace.Default.First().Name.ShouldBe(shortcutName);
+            Workspace.Default.First().Link.ShouldBe(shortcutLink);
+        }
+    }
+}
