b87e27286f656e909d0c2d2ff3b3f102925eaf3a
Jack Dewinter <Jack.Dewinter@red-gate.com>
2019-03-17 21:23:37 +0000

    Add shortcut service and shortcut.

---
+++ ShortcutManager/Shortcut.cs
@@ -0,0 +1,14 @@
+﻿namespace ShortcutManager
+{
+    internal class Shortcut
+    {
+        public string Name { get; }
+        public string Link { get; }
+
+        public Shortcut(string name, string link)
+        {
+            Name = name;
+            Link = link;
+        }
+    }
+}
---
+++ ShortcutManager/ShortcutService.cs
@@ -0,0 +1,11 @@
+﻿namespace ShortcutManager
+{
+    internal class ShortcutService
+    {
+        public void AddShortcut(string name, string link)
+        {
+            var shortcut = new Shortcut(name, link);
+            Workspace.Default.Add(shortcut);
+        }
+    }
+}
