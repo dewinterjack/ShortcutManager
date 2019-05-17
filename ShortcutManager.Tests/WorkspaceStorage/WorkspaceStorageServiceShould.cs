dd184c5db8a8c4a95bc06b575832c5b574a9cb52
Jack Dewinter <jack.dewinter@red-gate.com>
2019-05-17 15:30:24 +0100

    Test workspace formatting on save

---
+++ ShortcutManager.Tests/WorkspaceStorage/WorkspaceStorageServiceShould.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+using ShortcutManager.WorkspaceStorage;
+
+namespace ShortcutManager.Tests.WorkspaceStorage
+{
+    internal class WorkspaceStorageServiceShould
+    {
+        [Test]
+        public void SaveWorkspaceWithFormatting()
+        {
+            var textFileWriter = Substitute.For<ITextWriter>();
+            var workspace = new Workspace("MyWorkspace");
+            workspace.Shortcuts.Add(new Shortcut("SomeShortcut", "SomeLink"));
+            workspace.Shortcuts.Add(new Shortcut("AnotherShortcut", "AnotherLink"));
+
+            var workspaceStorageService = new WorkspaceStorageService(textFileWriter);
+
+            workspaceStorageService.Save(workspace);
+            textFileWriter.Received().WriteToStorage(Arg.Is<List<string>>(x =>
+                x.Contains("MyWorkspace") && x.Contains("Name: SomeShortcut | Link: SomeLink") &&
+                x.Contains("Name: AnotherShortcut | Link: AnotherLink")));
+        }
+    }
+}
